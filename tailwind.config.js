940fc5b217d59be078a57e282d81159dfa7c2e59
Ratan-Das <ratandas8820@gmail.com>
2023-08-30 10:04:52 +0600

    te house project 95% complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                THbg1: '#FF8938',
+                THbg2: '#F00',
+            }
+        },
+    },
+    plugins: [],
+}
